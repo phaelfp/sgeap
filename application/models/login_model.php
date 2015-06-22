58bb49225aec1484f71731f4a0c79f2410814a5c
Raphael Freitas Pereira <phael.rj@gmail.com>
2015-06-22 15:10:58 -0300

    correção da consulta sql no login

--- application/models/login_model.php
+++ application/models/login_model.php
@@ -5,7 +5,7 @@ class login_model extends CI_Model{
 		$sql = "SELECT p.nome, p.login 
 			FROM Pessoa p
 			WHERE p.login='".$this->db->escape_str($username)."' AND 
-				p.pass = '".$this->db->escape_str(sha1($password))."'";
+				p.password = '".$this->db->escape_str(sha1($password))."'";
         
 		$query = $this->db->query($sql);
 		return $query->row();
